55b2a2768aa29a15630e0620267a6230223fe829
chumpon-asaneerat <chumponsenate@gmail.com>
2019-01-24 16:07:19 +0700

    build file (gulp) and add assets.

--- dist/server/js/nlib-express.js
+++ dist/server/js/nlib-express.js
@@ -7,23 +7,338 @@ process.env['ROOT_PATHS'] = path.dirname(require.main.filename);
 const rootPath = process.env['ROOT_PATHS'];
 
 const express = require('express');
-// middlewares for expressjs.
-const favicon = require('serve-favicon');
-const logger = require('morgan');
+
+//#region NExpressModule and related classes
+
+//#region NExpressModule (namespace)
+
+class NExpressModule { }
+
+//#endregion
+
+//#region Express Middleware modules
+
+//#region Helmet
+
 const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
+
+/**
+ * Helmet.
+ */
+NExpressModule.Helmet = class {
+    static init(server) {
+        if (!server || !server.app) return;
+        // helmet.
+        server.app.use(helmet());
+    };
+};
+
+//#endregion
+
+//#region Logger
+
+const logger = require('morgan');
+
+/**
+ * Logger (morgan).
+ */
+NExpressModule.Logger = class {
+    static init(server) {
+        if (!server || !server.app) return;
+        // logger (morgan).
+        server.app.use(logger('dev'));
+    };
+};
+
+//#endregion
+
+//#region Body Parser
+
 const bodyParser = require('body-parser');
 
-// view engine for expressjs.
-const exphbs = require('express-handlebars');
+/**
+ * Body Parser.
+ */
+NExpressModule.BodyParser = class {
+    static init(server) {
+        if (!server || !server.app) return;
+        // body parser.
+        server.app.use(bodyParser.json());
+        // extended must be true. If extended is false, you can not post "nested object".
+        server.app.use(bodyParser.urlencoded({ extended: true }));
+    };
+};
+
+//#endregion
+
+//#region Cookie Parser
+
+const cookieParser = require('cookie-parser');
+
+/**
+ * Cookie Parser.
+ */
+NExpressModule.CookieParser = class {
+    static init(server) {
+        if (!server || !server.app) return;
+        // cookie parser.
+        server.app.use(cookieParser());
+    };
+};
+
+//#endregion
+
+//#region Static Paths
+
+/**
+ * Static Paths.
+ */
+NExpressModule.StaticPaths = class {
+    static init(server) {
+        if (!server || !server.app) return;
+        console.log('init common static paths....');
+        // max-age variables
+        let libMaxAge = { maxage: '15s' };
+        // path variables
+        let publicPath = server.opts.paths.public;
+        let commonPath = path.join(publicPath, 'lib');
+        let assetPath = path.join(publicPath, 'assets');
+        let imagePath = path.join(assetPath, 'images');
+        let videoPath = path.join(assetPath, 'videos');
+        let audioPath = path.join(assetPath, 'audios');        
+
+        // common lib paths.
+        server.app.use('/lib', express.static(commonPath, libMaxAge));
+        // public paths.
+        server.app.use('/public', express.static(publicPath));
+        // public->assets paths.
+        server.app.use('/images', express.static(imagePath));
+        server.app.use('/videos', express.static(videoPath));
+        server.app.use('/audios', express.static(audioPath));
+    };
+};
+
+//#endregion
+
+//#region Third Party Lib Paths
+
+/**
+ * ThirdParty Lib Paths.
+ */
+NExpressModule.ThirdPartyLibPaths = class {
+    static init(server) {
+        if (!server || !server.app) return;
+        // max-age variables
+        let distMaxAge = { maxage: '1d' };
+        // path variables
+        let cfgPath = server.opts.paths.config;
+        let publicPath = server.opts.paths.public;
+        let distPath = path.join(publicPath, 'dist');
+        let distCfgFile = path.join(cfgPath, 'dist.json');
+
+        try {
+            let obj = fs.readFileSync(distCfgFile, 'utf8');
+            if (obj) {
+                let dist_libs = JSON.parse(obj);
+                dist_libs.forEach(el => {
+                    let localPath = el.path;
+                    console.log('publish `' + localPath + '`');                    
+                    let eachPath = path.join(distPath, localPath);
+                    server.app.use(el.route, express.static(eachPath, distMaxAge));
+                });
+            }
+        }
+        catch (err) {
+            console.error(err);
+        };
+    };
+};
+
+//#endregion
+
+//#region FavIcon
+
+const favicon = require('serve-favicon');
+
+/**
+ * favicon.
+ */
+NExpressModule.FavIcon = class {
+    static init(server) {
+        if (!server || !server.app) return;
+        // if change favicon.ico required to restart server.
+        let iconFile = path.join(server.opts.paths.public, 'favicon.ico');
+        fs.exists(iconFile, (found) => {
+            if (found) {
+                server.app.use(favicon(iconFile));
+            }
+            else {
+                console.log(favicon, 'not found.');
+            }
+        });
+    };
+};
+
+//#endregion
+
+//#endregion
+
+//#region View Engines
+
+//#region NExpressViewModule (Base class)
+
+NExpressModule.NExpressViewModule = class {
+    /**
+     * Constructor.
+     * @param {Any} server 
+     */
+    constructor(server) {
+        this._server = server;
+    };
+    /**
+     * Use View Engine.
+     */
+    use() { };
+    /**
+     * Render view.
+     * @param {Any} res 
+     * @param {String} viewFile 
+     * @param {Object} opts 
+     */
+    render(res, viewFile, opts) {
+        if (!this._server || !this._server.app) return;        
+        // use current view engine.
+        this.use();
+        // render file.
+        let viewPath = this._server.opts.paths.views;
+        let target = path.join(viewPath, viewFile);
+        res.render(target, opts);
+    };
+    /**
+     * Gets server instance.
+     */
+    get server() { return this._server; }
+}
+
+//#endregion
+
+//#region Handlebar
+
+const exphbs = require('express-handlebars'); // for HBS (Handlebars)
 const hbs = exphbs.create();
 
-const cons = require('consolidate');
+/**
+ * Handlebar.
+ */
+NExpressModule.Handlebar = class extends NExpressModule.NExpressViewModule {
+    /**
+     * Use View Engine.
+     */
+    use() {
+        // seting engine.
+        this.server.app.engine('handlebars', hbs.engine)
+        // use view engine
+        this.server.app.set('view engine', 'handlebars');
+    };
+    /**
+     * Init and gets View renderer instance.
+     * @param {Any} server 
+     */
+    static init(server) {
+        return new NExpressModule.Handlebar(server);
+    };
+};
+
+//#endregion
+
+//#region EJS
+
+/**
+ * EJS.
+ */
+NExpressModule.EJS = class extends NExpressModule.NExpressViewModule {
+    /**
+     * Use View Engine.
+     */
+    use() {
+        // use view engine
+        this.server.app.set('view engine', 'ejs');
+    };
+    /**
+     * Init and gets View renderer instance.
+     * @param {Any} server 
+     */
+    static init(server) {
+        return new NExpressModule.EJS(server);
+    };
+};
+
+//#endregion
+
+//#region PUG/Jade
+
+/**
+ * PUG/Jade.
+ */
+NExpressModule.PUG = class extends NExpressModule.NExpressViewModule {
+    /**
+     * Use View Engine.
+     */
+    use() {
+        // use view engine
+        this.server.app.set('view engine', 'pug');
+    };
+    /**
+     * Init and gets View renderer instance.
+     * @param {Any} server 
+     */
+    static init(server) {
+        return new NExpressModule.PUG(server);
+    };
+};
+
+//#endregion
+
+//#region HTML
+
+const cons = require('consolidate'); // for HTML
+
+/**
+ * HTML.
+ */
+NExpressModule.HTML = class extends NExpressModule.NExpressViewModule {
+    /**
+     * Use View Engine.
+     */
+    use() {
+        // seting engine.
+        this.server.app.engine('html', cons.hogan);
+        // use view engine
+        this.server.app.set('view engine', 'html');
+    };
+    /**
+     * Init and gets View renderer instance.
+     * @param {Any} server 
+     */
+    static init(server) {
+        return new NExpressModule.HTML(server);
+    };
+};
+
+//#endregion
+
+//#endregion
+
+//#endregion
+
+//#region NWebServer
 
 /**
  * Express Web Server wrapper class.
  */
 class NWebServer {
+    //#region Constructor
+
     /**
      * Constructor.
      */
@@ -40,6 +355,7 @@ class NWebServer {
             },
             paths: {
                 root: rootPath,
+                config: path.join(rootPath, 'configs'),
                 public: path.join(rootPath, 'public'),
                 views: path.join(rootPath, 'views')
             }
@@ -47,16 +363,21 @@ class NWebServer {
         // express app instance.
         this._app = express();
         // init basic middlewares.
-        this._app.use(logger('dev')); // logger (morgan).
-        // helmet.
-        this._app.use(helmet());
-        // body parser.
-        this._app.use(bodyParser.json());
-        // extended must be true. If extended is false, you can not post "nested object".
-        this._app.use(bodyParser.urlencoded({ extended: true }));
-        // cookie parser.
-        this._app.use(cookieParser());
+        NExpressModule.Logger.init(this); // Logger (morgan).        
+        NExpressModule.Helmet.init(this); // helmet.
+        NExpressModule.BodyParser.init(this); // body parser.
+        NExpressModule.CookieParser.init(this); // cookie parser.
+        // Init View Engine render.
+        this.HBS = NExpressModule.Handlebar.init(this); // HBS View Engine.
+        this.EJS = NExpressModule.EJS.init(this); // EJS View Engine.
+        this.PUG = NExpressModule.PUG.init(this); // PUG View Engine.
+        this.HTML = NExpressModule.HTML.init(this); // HTML View Engine.
     };
+
+    //#endregion
+
+    //#region GET/POST/ALL
+
     /**
      * GET Method.
      * 
@@ -64,12 +385,9 @@ class NWebServer {
      * @param {Express router callback} callback 
      */
     get() {
-        if (!this._app) return;
-        if (arguments.length >= 1) {
-            let isRouter = arguments[0] instanceof NWebRouter;
-            let router = (isRouter) ? arguments[0] : new NWebRouter(arguments[0], arguments[1]);
-            this._app.get(router.url, router.execute);
-        }
+        let router = NWebRouter.parse(...arguments);
+        if (!router) return;
+        this._app.get(router.url, router.execute);
     };
     /**
      * POST Method.
@@ -78,12 +396,9 @@ class NWebServer {
      * @param {Express router callback} callback 
      */
     post() {
-        if (!this._app) return;
-        if (arguments.length >= 1) {
-            let isRouter = arguments[0] instanceof NWebRouter;
-            let router = (isRouter) ? arguments[0] : new NWebRouter(arguments[0], arguments[1]);
-            this._app.post(router.url, router.execute);
-        }
+        let router = NWebRouter.parse(...arguments);
+        if (!router) return;
+        this._app.post(router.url, router.execute);
     };
     /**
      * ALL Methods.
@@ -91,40 +406,36 @@ class NWebServer {
      * @param {String} url 
      * @param {Express router callback} callback 
      */
-    all(url, callback) {
-        if (!this._app) return;
-        if (arguments.length >= 1) {
-            let isRouter = arguments[0] instanceof NWebRouter;
-            let router = (isRouter) ? arguments[0] : new NWebRouter(arguments[0], arguments[1]);
-            this._app.all(router.url, router.execute);
-        }
+    all() {
+        let router = NWebRouter.parse(...arguments);
+        if (!router) return;
+        this._app.all(router.url, router.execute);
     };
+
+    //#endregion
+    
+    //#region Start
+
     /**
      * Start server.
      */
     start() {
-        if (!this._app) {
-            console.log('Express app instance is not assigned.');
-            return;
-        }
-        // if change favicon.ico required to restart server.
-        let iconFile = path.join(this._opts.paths.public, 'favicon.ico');
-        fs.exists(iconFile, (found) => {
-            if (found) {
-                this._app.use(favicon(iconFile));
-            }
-            else {
-                console.log(favicon, 'not found.');
-            }
-        });
+        NExpressModule.StaticPaths.init(this);
+        NExpressModule.FavIcon.init(this);
+        NExpressModule.ThirdPartyLibPaths.init(this);
         // setup port
         this._app.set('port', process.env.PORT || this._opts.server.port);
         // start server.
-        let self = this;
-        let server = self._app.listen(self._app.get('port'), () => {
-            console.log(self.info + ' listening on port `' + server.address().port + '`');
+        let msg = this.info;
+        let server = this._app.listen(this._app.get('port'), () => {
+            console.log(msg + ' listening on port `' + server.address().port + '`');
         });
     };
+
+    //#endregion
+
+    //#region public properties
+
     /**
      * Gets options.
      */
@@ -137,9 +448,15 @@ class NWebServer {
      * Get app information
      */
     get info() { return '`' + this._opts.app.name + '` v' + this._opts.app.version; }
+
+    //#endregion
 };
 
 exports.NWebServer = NWebServer;
+
+//#endregion
+
+//#region NWebRouter
 
 /**
  * Express Web Route mapping class.
@@ -164,9 +481,9 @@ class NWebRouter {
     get execute() { 
         let self = this;
         let fn = (req, res, next) => {
-            console.log('Request:', req.url);
+            //console.log('Request:', req.url);
             if (!self._callback) {
-                console.log('Request:', req.url, 'callback not assigned.');
+                //console.log('Request:', req.url, 'callback not assigned.');
                 if (next) next();
             }
             else {
@@ -175,106 +492,19 @@ class NWebRouter {
         }
         return fn;
     }
+    /**
+     * Parse arguments for NWebRouter instance.
+     */
+    static parse() {
+        let router = null;
+        if (arguments.length >= 1) {
+            let isRouter = arguments[0] instanceof NWebRouter;
+            router = (isRouter) ? arguments[0] : new NWebRouter(arguments[0], arguments[1]);            
+        }
+        return router;
+    };
 };
 
 exports.NWebRouter = NWebRouter;
 
-/**
- * Base express view engine renderer.
- */
-class NWebRenderer {
-    constructor(server) {
-        this._server = server;
-        this._app = (server) ? server.app : null;
-    };
-    /**
-     * Setup view engine.
-     * 
-     * @param {*} app The Express App.
-     */
-    setup(app) { };
-    /**
-     * Render view file.
-     * 
-     * @param {Object} res The response object.
-     * @param {String} path The view path (relative with views's path).
-     * @param {Object} opts The view render options.
-     */
-    render(res, viewFile, opts) {
-        if (!this._app) {
-            res.status(501).send('Internal Error. Express app instance not found.');
-        }
-        this.setup(this._app);
-        let target = path.join(this.viewPath, viewFile);
-        res.render(target, opts);
-    };
-    /**
-     * Gets server instance.
-     */
-    get server() { return this._server; }
-    /**
-     * Gets Express app instance.
-     */
-    get app() { return this._app; }
-    /**
-     * Gets Views's path.
-     */
-    get viewPath() { return this._server.opts.paths.views; };
-};
-
-exports.NWebRenderer = NWebRenderer;
-
-/**
- * The Handlebar Renderer.
- */
-NWebRenderer.HBS = class extends NWebRenderer {
-    /**
-     * setup view engine.
-     * @param {*} app The Express App.
-     */
-    setup(app) {
-        if (!app) return;
-        app.engine('handlebars', hbs.engine)
-        app.set('view engine', 'handlebars');
-    };
-};
-/**
- * The EJS Renderer.
- */
-NWebRenderer.EJS = class extends NWebRenderer {
-    /**
-     * setup view engine.
-     * @param {*} app The Express App.
-     */
-    setup(app) {
-        if (!app) return;
-        app.set('view engine', 'ejs');
-    };
-};
-/**
- * The PUG Renderer.
- */
-NWebRenderer.PUG = class extends NWebRenderer {
-    /**
-     * setup view engine.
-     * @param {*} app The Express App.
-     */
-    setup(app) {
-        if (!app) return;
-        app.set('view engine', 'pug');
-    };
-};
-/**
- * The HTML Renderer.
- */
-NWebRenderer.HTML = class extends NWebRenderer {
-    /**
-     * setup view engine.
-     * @param {*} app The Express App.
-     */
-    setup(app) {
-        if (!app) return;
-        app.engine('html', cons.hogan);
-        app.set('view engine', 'html');
-    };
-};
+//#endregion
