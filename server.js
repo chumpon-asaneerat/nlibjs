7d4afb3de8251132a76dc3c5a1db125e99793a57
chumpon-asaneerat <chumponsenate@gmail.com>
2019-01-24 14:58:11 +0700

    Refactor nlib-express.js

--- server.js
+++ server.js
@@ -10,12 +10,6 @@ server.opts.app.version = '0.0.1';
 server.opts.app.lastupdated = '2019-01-25';
 server.opts.server.port = '3000';
 
-const NRenderer = wserv.NWebRenderer;
-const HBS = new NRenderer.HBS(server);
-const EJS = new NRenderer.EJS(server);
-const PUG = new NRenderer.PUG(server);
-const HTML = new NRenderer.HTML(server);
-
 class NPage {
     excute(req, res, next) { 
         res.send('`' + req.url + '` NPage execute success!');
@@ -24,7 +18,7 @@ class NPage {
 
 NPage.home = class extends NPage {
     excute(req, res, next) {
-        HTML.render(res, 'examples/home');
+        server.HTML.render(res, 'examples/home');
     }
 };
 
@@ -38,27 +32,28 @@ let routes = [
    new NWebRouter('/home', new NPage.home().excute),
    /*
     new NWebRouter('/home', (req, res) => { 
-        HTML.render(res, 'examples/home');
+        server.HTML.use();
+        server.render(res, 'examples/home');
     }),
     */
     new NWebRouter('/about', (req, res) => { 
-        PUG.render(res, 'examples/about', { title:'About page.', msg: 'This is about page.' });
+        server.PUG.render(res, 'examples/about', { title:'About page.', msg: 'This is about page.' });
     }),
     new NWebRouter('/contact', (req, res) => { 
-        EJS.render(res, 'examples/contact', { msg: 'This is Contact page.' });
+        server.EJS.render(res, 'examples/contact', { msg: 'This is Contact page.' });
     }),
     new NWebRouter('/supports', (req, res) => { 
         res.send('`/supports` Success!'); 
     }),
     new NWebRouter('/login')
-]
+];
 
 routes.forEach(route => { 
     server.get(route);
 });
 
 server.get('/logout', (req, res) => {
-    HBS.render(res, 'examples/logout', { msg: 'logout page' });
+    server.HBS.render(res, 'examples/logout', { msg: 'logout page' });
 });
 
 server.start();
